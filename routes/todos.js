347043d744edb76c582b52c822a0febd8ace39d9
unknown <sogand.azadi82@gmail.com>
2024-08-04 20:53:17 -0700

    first test passes

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
+
+const todos = [{id: 1, name: "Do something" , completed: false}]
 
 router.get("/" , function(req , res , next ){
-    res.send("respond with a resource");
+    res.json(todos);
 })
 
 module.exports = router;
